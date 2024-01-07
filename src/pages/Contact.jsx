f0b5111e897e7882a789642dc2bf4048c7f43f52
youxt-njnu <10190208@njnu.edu.cn>
2024-01-07 22:15:38 +0800

    0107night

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,23 +1,61 @@
-import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+import { Canvas } from '@react-three/fiber';
+import React, { Suspense, useRef, useState } from 'react';
+import Loader from '../components/Loader';
+import Fox from '../models/Fox';
 
 const Contact = () => {
   const formRef = useRef(null);
-  const [form, setForm] = useState({ name: '', email: '', message: '' });
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
   const [isLoading, setIsLoading] = useState(false);
+  const [currentAnimation, setCurrentAnimation] = useState("idle");
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value })
   };
-  const handleFocus = () => { };
-  const handleBlur = () => { };
-  const handleSubmit = () => { };
+  const handleFocus = () => { setCurrentAnimation('walk') };
+  const handleBlur = () => { setCurrentAnimation('idle') };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setCurrentAnimation('hit');
+
+    emailjs.send(
+      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      {
+        from_name: form.name,
+        to_name: "Shealin",
+        from_email: form.email,
+        to_email: 'yikeyouIl@gmail.com',
+        message: form.message,
+      },
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+    ).then(() => {
+      setIsLoading(false);
+
+      //TODO: show success message
+      //TODO: hide an alert
+      setTimeout(() => {
+        setCurrentAnimation('idle');
+        setForm({ name: "", email: "", message: "" });
+      }, [3000]);
+
+
+    }).catch((error) => {
+      setIsLoading(false);
+      setCurrentAnimation('idle');
+      console.log(error);
+      //TODO: show error message 
+    });
+  };
 
   return (
     <section className='relative flex lg:flex-row flex-col max-container'>
       <div className='flex-1 min-w-[50%] flex flex-col'>
         <h1 className="head-text">Get in Touch</h1>
 
-        <form action="" className="w-full flex flex-col gap-7 mt-14"
+        <form action="" className="w-full flex flex-col gap-7 mt-14" ref={formRef}
           onSubmit={handleSubmit}>
           <label className='text-black-500 font-semibold'>
             Name
@@ -50,8 +88,8 @@ const Contact = () => {
           <label className='text-black-500 font-semibold'>
             Your Message
             <textarea
-              type="message"
-              rows={4}
+              name="message"
+              rows='4'
               className='textarea'
               placeholder='Let me know how I can help you!'
               required
@@ -67,9 +105,29 @@ const Contact = () => {
             disabled={isLoading}
             onFocus={handleFocus}
             onBlur={handleBlur}>
-            {isLoading ? 'Sending...' : 'Send Message'}
+            {isLoading ? 'Sending...' : 'Submit'}
           </button>
         </form>
+      </div>
+
+      <div className='lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]'>
+        <Canvas camera={{
+          position: [0, 0, 5],
+          fov: 75,
+          near: 0.1,
+          far: 1000
+        }}>
+          <ambientLight intensity={0.5} />
+          <directionalLight intensity={2.5} position={[0, 0, 1]} />
+          <Suspense fallback={<Loader />}>
+            <Fox
+              currentAnimation={currentAnimation}
+              position={[0.5, 0.35, 0]}
+              rotation={[12.6, -0.5, 0]}
+              scale={[0.5, 0.5, 0.5]}
+            />
+          </Suspense>
+        </Canvas>
       </div>
     </section>
   )
